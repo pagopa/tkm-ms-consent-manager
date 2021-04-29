e6b0fa6bf7ca3c120c1cafc7bac1550bd9ebd207
Giuseppe Ferro <giuseppe.ferro@gft.com>
2021-04-29 14:21:30 +0200

    paopa-939 added openapi spec v1.0.0
    .

--- src/main/java/it/gov/pagopa/tkm/ms/consentmanager/repository/CardRepository.java
+++ src/main/java/it/gov/pagopa/tkm/ms/consentmanager/repository/CardRepository.java
@@ -3,6 +3,6 @@ package it.gov.pagopa.tkm.ms.consentmanager.repository;
 import it.gov.pagopa.tkm.ms.consentmanager.model.entity.*;
 import org.springframework.data.jpa.repository.*;
 
-public interface InstrumentRepository extends JpaRepository<Card, Long> {
+public interface CardRepository extends JpaRepository<Card, Long> {
 
 }
