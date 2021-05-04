9852f622afc8451d7c20bd71839e8b51620ee935
Giuseppe Ferro <giuseppe.ferro@gft.com>
2021-05-04 16:22:15 +0200

    added repository

--- src/main/java/it/gov/pagopa/tkm/ms/consentmanager/config/ConsentManagerApplication.java
+++ src/main/java/it/gov/pagopa/tkm/ms/consentmanager/config/ConsentManagerApplication.java
@@ -1,13 +1,10 @@
 package it.gov.pagopa.tkm.ms.consentmanager.config;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class ConsentManagerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConsentManagerApplication.class, args);
+
 	}
 
 }
