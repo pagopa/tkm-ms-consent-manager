490bfe8a2ea340c744da734dd75e90e4baaf93c3
David Bertoni <David Bertoni>
2021-05-25 11:40:22 +0200

    PagoPA-936 - Integrating Tkm-common
    .
    
    .
    
    .
    
    .
    
    .
    
    .

--- src/main/java/it/gov/pagopa/tkm/ms/consentmanager/ConsentManagerApplication.java
+++ src/main/java/it/gov/pagopa/tkm/ms/consentmanager/ConsentManagerApplication.java
@@ -1,11 +1,14 @@
 package it.gov.pagopa.tkm.ms.consentmanager;
 
+import it.gov.pagopa.tkm.config.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.*;
+import org.springframework.context.annotation.*;
 
 @SpringBootApplication
 @EnableCaching
+@Import(CustomAnnotation.class)
 public class ConsentManagerApplication {
 
 	public static void main(String[] args) {
