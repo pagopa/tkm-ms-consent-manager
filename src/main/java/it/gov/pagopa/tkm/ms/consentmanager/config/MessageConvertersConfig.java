0344b7e0416e470ec5f65ce5f7442dccc1df7773
Roberto Rasi <roberto.rasi@gft.com>
2022-04-22 18:48:51 +0200

    Test run sit

--- src/main/java/it/gov/pagopa/tkm/ms/consentmanager/config/MessageConvertersConfig.java
+++ src/main/java/it/gov/pagopa/tkm/ms/consentmanager/config/MessageConvertersConfig.java
@@ -1,13 +1,14 @@
 package it.gov.pagopa.tkm.ms.consentmanager.config;
 
-import org.jetbrains.annotations.*;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.*;
-import org.springframework.http.converter.json.*;
-import org.springframework.http.converter.xml.*;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.converter.xml.Jaxb2RootElementHttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.*;
+import javax.validation.constraints.NotNull;
+import java.util.List;
 
 @EnableWebMvc
 @Configuration
